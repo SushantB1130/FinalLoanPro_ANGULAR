eda4f82498305480556e1b823d1a059c5db633f9
AbhiiBhintade <abhiiab705708@gmail.com>
2023-09-01 10:31:28 +0530

    End

--- FINAL_LOAN_PROJECT/src/app/modules/account/ledger/ledger.component.ts
+++ FINAL_LOAN_PROJECT/src/app/modules/account/ledger/ledger.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoanserviceService } from 'src/app/service/loanservice.service';
 
 @Component({
   selector: 'app-ledger',
   templateUrl: './ledger.component.html',
   styleUrls: ['./ledger.component.css']
 })
-export class LedgerComponent {
+export class LedgerComponent implements OnInit {
+
+  constructor(private ls:LoanserviceService) {}
+
+  customers:any[];
+
+  
+  customerstatus:string='DISBURSED';
+  customerstatus2:string="no";
+
+  ngOnInit(): void {
+    
+    this.getcustomers();
+  }
+
+  getcustomers()
+  {
+    this.ls.getcustomers(this.customerstatus,this.customerstatus2).subscribe((data:any)=>{
+      this.customers=data.responsedata;
+      
+    })
+  }
+
+  createLedger(customerId:number)
+  {
+    this.ls.initiateCPV(customerId).subscribe((data:any)=>{
+      window.location.reload();
+    })
+  }
+
 
 }
